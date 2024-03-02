d34da3833da2ed42ab3df48c541cdbd14c68a62e
mik2003 <62058516+mik2003@users.noreply.github.com>
2024-03-02 14:04:20 +0100

    Update AndroidManifes

--- app/src/main/java/com/mik2003/boukennihon/ui/map/MapFragment.kt
+++ app/src/main/java/com/mik2003/boukennihon/ui/map/MapFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import android.webkit.WebView
 import com.mik2003.boukennihon.databinding.FragmentMapBinding
 
